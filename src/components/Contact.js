1f0d1ac1a2d585a0688939729c54c419ea8fe340
Donkey <danglayers@gmail.com>
2021-03-29 13:16:15 -0400

    initial commit

---
+++ src/components/Contact.js
@@ -0,0 +1,180 @@
+import React from "react";
+import Particles from "react-particles-js";
+import { isMobile } from "react-device-detect";
+const particleParam = {
+  particles: {
+    number: {
+      value: 100,
+    },
+    color: {
+      value: "#fff",
+    },
+    shape: {
+      type: "circle",
+      stroke: {
+        width: 0,
+        color: "#000000",
+      },
+      polygon: {
+        nb_sides: 5,
+      },
+    },
+    opacity: {
+      value: 0.5,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false,
+      },
+    },
+    size: {
+      value: 10,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 40,
+        size_min: 0.1,
+        sync: false,
+      },
+    },
+    line_linked: {
+      enable: false,
+    },
+    move: {
+      enable: true,
+      speed: 1,
+      direction: "top",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+    },
+  },
+  retina_detect: true,
+};
+
+export default class MyForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.submitForm = this.submitForm.bind(this);
+    this.state = {
+      status: "",
+    };
+  }
+
+  componentDidMount() {}
+
+  submitForm(ev) {
+    ev.preventDefault();
+    const form = ev.target;
+    const data = new FormData(form);
+    const xhr = new XMLHttpRequest();
+    xhr.open(form.method, form.action);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        form.reset();
+        this.setState({ status: "SUCCESS" });
+      } else {
+        this.setState({ status: "ERROR" });
+      }
+    };
+    xhr.send(data);
+  }
+
+  render() {
+    const { status } = this.state;
+    return (
+      <div className="contact" id={this.props.id}>
+        {!isMobile ? (
+          <Particles className="particle-background" params={particleParam} />
+        ) : (
+          <div className="mobile-particles"></div>
+        )}
+        <div
+          className={!isMobile ? "particle-cover" : "mobile-particle-cover"}
+          id="particle-cover"
+        >
+          <div className="particles-container">
+            <form
+              onSubmit={this.submitForm}
+              action="https://formspree.io/f/mwkaerjn"
+              method="POST"
+              className="contact-form"
+            >
+              <div className="section-heading--about">
+                <h1>Contact Me</h1>
+              </div>
+              <div className="contact-form-container">
+                <div className="contact-input">
+                  <label>Your name:</label>
+                  <input
+                    type="text"
+                    name="name"
+                    placeholder=" Enter your name"
+                  />
+                  <label>Your email:</label>
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder=" Enter your email"
+                  />
+                  <label>Your message:</label>
+                  <textarea
+                    name="message"
+                    placeholder=" Enter your message"
+                    className="message"
+                  ></textarea>
+                  {status === "SUCCESS" ? (
+                    <p>Thanks! I'll get back to you as soon as possible.</p>
+                  ) : (
+                    <button className="submit-button">Submit</button>
+                  )}
+                  {status === "ERROR" && <p>Ooops! There was an error.</p>}
+                </div>
+                <div className="contact-information">
+                  <p>
+                    Want to get in touch? Questions on my projects? Job
+                    recruitment? Feel free to contact me through here.
+                    <br />
+                    <br />
+
+                    Simply click on the Github or LinkedIn logo below to check
+                    out my profile!
+                  </p>
+                  <div className="contact-socials">
+                    <a
+                      rel="noopener noreferrer"
+                      className="contact-social-item"
+                      href="https://github.com/danayers95"
+                      target="_blank"
+                    >
+                      <img
+                        src={require("../images/github.svg")}
+                        alt="github"
+                      ></img>
+                    </a>
+                    <a
+                      rel="noopener noreferrer"
+                      className="contact-social-item"
+                      href="https://www.linkedin.com/in/danayers95/"
+                      target="_blank"
+                    >
+                      <img
+                        src={require("../images/linkedin.svg")}
+                        alt="linkedin"
+                      ></img>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/components/Projects.js
@@ -0,0 +1,135 @@
+import React from "react";
+import Project from "./Project";
+import game from '../images/game.png';
+import EmployeeDirectory from '../images/EmployeeDirectory.png';
+import ArtistSavor from '../images/ArtistSavor.png';
+import ReactPortfolio from '../images/ReactPortfolio.png';
+// represents the whole entire projects section
+const Projects = (props) => (
+  <div className="projects">
+    <div className="projects-main">
+      <div className="content-container">
+        <div id={props.id} className="section-heading">
+          <h1>Projects</h1>
+        </div>
+        <div className="projects-content">
+          <Project
+            imgSrc={game}
+            title="2048"
+            shortDescription={
+              <p>A simple recreation of the mobile game 2048.</p>
+            }
+            longDescription={`<h3 class="modal-subtitle">Description</h3>
+                        <p>You need to move the tiles and every time you move one, another tile pops up in a random manner 
+                        anywhere in the box. When two tiles with the same number on them collide with one another as you move them, they will merge into one 
+                        tile with the sum of the numbers written on them initially.</p>
+                        <h3 class="modal-subtitle">Links</h3>
+                        <p>
+                        <a href="https://lit-garden-06285.herokuapp.com/" class="demo">Demo</a>
+                        </p>
+                        <p>
+                        <a href="https://github.com/danayers95/2048-Game" class="demo">Github</a>
+                        </p>
+                        <h3 class="modal-subtitle">Technology</h3>
+                        <ul class="technology">
+                            <li>ReactJS</li>
+                            <li>SCSS</li>
+                            <li>HTML</li>
+                            <li>CSS</li>
+                            <li>Heroku</li>
+                        </ul>
+                        `}
+          />
+          <Project
+            imgSrc={EmployeeDirectory}
+            title="Employee Directory"
+            shortDescription={
+              <p>
+                An application that generates a directory of employees to manage.
+              </p>
+            }
+            longDescription={`<h3 class="modal-subtitle">Description</h3>
+                        <p></p>
+                        <h3 class="modal-subtitle">Links</h3>
+                        <p>
+                        <a href="https://limitless-ravine-24473.herokuapp.com/" class="demo">Demo</a>
+                        </p>
+                        <p>
+                        <a href="https://github.com/danayers95/React-User-Directory" class="demo">Github</a>
+                        </p>
+                        <h3 class="modal-subtitle">Technology</h3>
+                        <ul class="technology">
+                            <li>HTML</li>
+                            <li>CSS</li>
+                            <li>Javascript</li>
+                            <li>Heroku</li>
+                            
+                        </ul>
+                        `}
+          />
+
+          <Project
+            imgSrc={ArtistSavor}
+            title="Artist Savor"
+            shortDescription={
+              <p>
+                Allows artists to stow away money for a rainy day!
+              </p>
+            }
+            longDescription={`<h3 class="modal-subtitle">Description</h3>
+                        <p>This project is primarily aimed at those who make income from music, art, or anything similar. Users 
+                        create an account and can deposit money earned from performances. The application also allows the user to track 
+                        any and all expenses for efficient money management.
+                        </p>
+                        <h3 class="modal-subtitle">Links</h3>
+                        <p>
+                        <a href="https://morning-fortress-86194.herokuapp.com" class="demo">Demo</a>
+                        </p>
+                        <p>
+                        <a href="https://github.com/danayers95/Artist-Savor-1" class="demo">Github></a>
+                        </p>
+                        <h3 class="modal-subtitle">Technology</h3>
+                        <ul class="technology">
+                            <li>Javascript</li>
+                            <li>CSS</li>
+                            <li>Handlebars</li>
+                            <li>Heroku</li>
+                        </ul>
+                        `}
+          />
+          <Project
+            imgSrc={ReactPortfolio}
+            title="React Portfolio"
+            shortDescription={
+              <p>
+                A portfolio to show my design skill as well as some of my projects. You are here.
+              </p>
+            }
+            longDescription={`<h3 class="modal-subtitle">Description</h3>
+                        <p>While I had already made myself a simple portfolio in React, I knew I wanted something that really showcased
+                        not only my ability to code but my ability to create a charming and fun application. I had to learn how to implement a lot 
+                        of new things for this project (such as the parallaxes and particle effects), and what resulted is a much better portfolio page than before.</p>
+                        <h3 class="modal-subtitle">Links</h3>
+                        <p>
+                        <a href="" class="demo">Demo</a>
+                        </p>
+                        <p>
+                        <a href="https://github.com/danayers95/Personal-Portfolio" class="demo">Github</a>
+                        </p>
+                        <h3 class="modal-subtitle">Technology</h3>
+                        <ul class="technology">
+                            <li>HTML</li>
+                            <li>CSS</li>
+                            <li>Javascript</li>
+                            <li>ReactJS</li>
+                            <li>Heroku</li>
+                        </ul>
+                        `}
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Projects;
