c292f14392e705d50fe83b909e79d10374527357
Donkey <danglayers@gmail.com>
2021-03-29 12:44:46 -0400

    updated credits

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -19,15 +19,14 @@ class Footer extends React.Component {
       showCloseButton: true,
       title: "Credits",
       html: `<ul>
-        <li><div>Icons (smiling sun, hummingbird) made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div></li>
+        <li><div>Contact form created from <a href="https://formspree.io/">Formspree</a></div></li>
         <li><div>Modals made by <a href="https://sweetalert2.github.io/">Sweet Alert</a></div></li>
         <li><div>Top name animation created with <a href="https://www.react-spring.io/">React Spring</a></div></li>
         <li><div>Top background made by <a href="https://codepen.io/alexzaworski/pen/mEkvAG">Alex Zaworski</a></div></li>
         <li><div>Bottom background from <a href="https://github.com/Wufe/react-particles-js#readme">react-particles-js</a></div></li>
         <li><div>Notification from <a href="https://github.com/minhtranite/react-notifications">react-notifications</a></div></li>
         <li><div>Cloud Parallax effect from <a href="https://matthew.wagerfield.com/parallax/">parallax.js</a></div></li>
-        <li><div>Contact form created from <a href="https://formspree.io/">Formspree</a></div></li>
-        <li><div>Last but not least, React from <a href="https://reactjs.org/">Facebook</a></div></li>
+        <li><div>React from <a href="https://reactjs.org/">Facebook</a></div></li>
         </ul>
         `,
       position: "top",
