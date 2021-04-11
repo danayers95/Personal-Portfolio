013878a9103ae2a7caaca5934cc6090e931441dc
Donkey <danglayers@gmail.com>
2021-04-11 16:07:53 -0400

    Updates

---
+++ precache-manifest.faf64d2c532c861d1b322c4f535c57ba.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ca28b4350144c5955a641ca09abea33",
+    "url": "/Personal-Portfolio/index.html"
+  },
+  {
+    "revision": "21679a45e9a88262599e",
+    "url": "/Personal-Portfolio/static/css/2.7c1e0511.chunk.css"
+  },
+  {
+    "revision": "344cc1a881e61bbd6318",
+    "url": "/Personal-Portfolio/static/css/main.d1c5496c.chunk.css"
+  },
+  {
+    "revision": "21679a45e9a88262599e",
+    "url": "/Personal-Portfolio/static/js/2.4b3eeba4.chunk.js"
+  },
+  {
+    "revision": "344cc1a881e61bbd6318",
+    "url": "/Personal-Portfolio/static/js/main.69fcef26.chunk.js"
+  },
+  {
+    "revision": "58e208618f91bd52ec16",
+    "url": "/Personal-Portfolio/static/js/runtime-main.9740f667.js"
+  },
+  {
+    "revision": "eb707b13f6d7d64e12bf813304db62b8",
+    "url": "/Personal-Portfolio/static/media/ArtistSavor.eb707b13.png"
+  },
+  {
+    "revision": "8a8d83de4a41b273592725fdeb2f1a93",
+    "url": "/Personal-Portfolio/static/media/CSS.8a8d83de.svg"
+  },
+  {
+    "revision": "973796a8e8ea3ec2ca0a52823eb6639a",
+    "url": "/Personal-Portfolio/static/media/EmployeeDirectory.973796a8.png"
+  },
+  {
+    "revision": "c27351c0bd78845ab90e1060d7b66c6c",
+    "url": "/Personal-Portfolio/static/media/HTML.c27351c0.svg"
+  },
+  {
+    "revision": "126bf5978c13389d1943c79e399c8621",
+    "url": "/Personal-Portfolio/static/media/MySQL.126bf597.svg"
+  },
+  {
+    "revision": "91eae1171f11c58ce0d4c46077bccc3b",
+    "url": "/Personal-Portfolio/static/media/React.91eae117.svg"
+  },
+  {
+    "revision": "1b8d8f174a439788796b9999f400e80f",
+    "url": "/Personal-Portfolio/static/media/ReactPortfolio.1b8d8f17.png"
+  },
+  {
+    "revision": "ffded2381d25dd906406d6e07f91dea5",
+    "url": "/Personal-Portfolio/static/media/bootstrap.ffded238.svg"
+  },
+  {
+    "revision": "227e7cfd16921b77727cdf985055e80d",
+    "url": "/Personal-Portfolio/static/media/game.227e7cfd.png"
+  },
+  {
+    "revision": "7447a07fb9a2554076b477be54b45c4c",
+    "url": "/Personal-Portfolio/static/media/github.7447a07f.svg"
+  },
+  {
+    "revision": "9f18301a14da523463e94a5fabc40bc4",
+    "url": "/Personal-Portfolio/static/media/handlebars.9f18301a.svg"
+  },
+  {
+    "revision": "2d2e31cc63a96c7e6044f52d30fd21b9",
+    "url": "/Personal-Portfolio/static/media/javascript.2d2e31cc.svg"
+  },
+  {
+    "revision": "55be353564668d29eb5469ea6cb7506d",
+    "url": "/Personal-Portfolio/static/media/linkedin.55be3535.svg"
+  },
+  {
+    "revision": "2985235df31b8742d893d5b7e80145c5",
+    "url": "/Personal-Portfolio/static/media/mongodb.2985235d.svg"
+  },
+  {
+    "revision": "68e8250c54ca6289bc68ef53bfcb10f0",
+    "url": "/Personal-Portfolio/static/media/nextjs.68e8250c.svg"
+  },
+  {
+    "revision": "6f7aeee8bb8ee61edfd134eef3152e22",
+    "url": "/Personal-Portfolio/static/media/npm.6f7aeee8.svg"
+  },
+  {
+    "revision": "43c9d125de48e59cb29a88dd1b20bc46",
+    "url": "/Personal-Portfolio/static/media/sequelize.43c9d125.svg"
+  }
+]);
